448d9282fbfb01370d400a9856e750d068548442
YY Boom <boom9yy@proton.me>
2023-06-26 14:28:54 -0300

    simple enemy

--- ball.lua
+++ ball.lua
@@ -26,6 +26,14 @@ function Ball:colide()
         self.yVel = collisionPosition * 5
     end
 
+    if checkCollision(self, AI) then
+        self.xVel = -self.speed
+        local middleBall = self.y + self.height / 2
+        local middleAI = AI.y + AI.height / 2
+        local collisionPosition = middleBall - middleAI
+        self.yVel = collisionPosition * 5
+    end
+
     if self.y < 0 then
         self.y = 0
         self.yVel = -self.yVel
@@ -33,6 +41,22 @@ function Ball:colide()
         self.y = love.graphics.getHeight() - self.height
         self.yVel = -self.yVel
     end
+
+    if self.x < 0 then
+        self.x = love.graphics.getWidth() / 2 - self.width / 2
+        self.y = love.graphics.getHeight() / 2 - self.height / 2
+        self.yVel = 0
+        self.xVel = self.speed
+    end
+
+    if self.x + self.width > love.graphics.getWidth() then
+        self.x = love.graphics.getWidth() / 2 - self.width / 2
+        self.y = love.graphics.getHeight() / 2 - self.height / 2
+        self.yVel = 0
+        self.xVel = -self.speed
+    end
+
+
 end
 
 
